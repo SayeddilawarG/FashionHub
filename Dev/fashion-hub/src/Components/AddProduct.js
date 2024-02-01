4dcb1b0ab6e755d7bcae32a75e37ddb2ece0c540
SayeddilawarG <sayed.dilawarg@gmail.com>
2024-02-02 00:31:08 +0530

    added update method

--- Dev/fashion-hub/src/Components/AddProduct.js
+++ Dev/fashion-hub/src/Components/AddProduct.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./AddProduct.css";
 import {Link} from "react-router-dom";
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 
 function AddProduct() {
+  const navigate = useNavigate();
   const { id } = useParams();
   console.log(id);
   const [showAddbtn,setShowAddbtn] = useState(true);
   const [isEdit,setIsEdit] = useState(false);
-  const [checkName,setCheckName] = useState();
-  const [checkDescription,setCheckDescription] = useState();
-  const [checkPrize,setCheckPrize] = useState();
+  const [errorMessage,setErrorMessage] = useState('');
+  const [isError,setIsError] = useState(false);
   const [productData,setProductData] = useState({
     name : "",
     description : "",
@@ -21,21 +21,17 @@ function AddProduct() {
   },[]);
 
   const getProductById=async (id)=>{
+    setIsEdit(true);
     const data = await fetch("http://localhost:5017/api/Product/GetProductByIdView/"+id);
     const json = await data.json();
     console.log("This is Get Product",json);
     setProductData(json);
   }
-
-  const showButton=()=>{
-    if(!(checkName =='' && checkDescription =='' && checkPrize =='')){
-      setShowAddbtn(false);
-    }
-  }
   const handleData=(e)=>{
     const newData = {...productData}
     newData[e.target.id] = e.target.value;
     setProductData(newData);
+    setIsError(false);
     console.log(newData);
   }
   const postData=async (e)=>{ 
@@ -52,18 +48,59 @@ function AddProduct() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body:JSON.stringify(payload)
+        body:JSON.stringify(payload)   
       }
     ).then(response=>{
       console.log(response);
+      if(response.ok){
+        navigate('/');
+        setIsError(false);
+      }
+      setIsError(true);
+      setErrorMessage("Product details are not valid")
     }).catch(error=>{
       console.log(error);
+      setIsError(true);
+      setErrorMessage("Product details are not valid")
+    })
+  }
+
+  const updateData = (e) =>{
+    e.preventDefault();
+    const payload = {
+      id : id,
+      name : productData.name,
+      description : productData.description,
+      prize : productData.prize
+    }
+
+    fetch('http://localhost:5017/api/Product/PutProduct',
+      {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(payload)   
+      }
+    ).then(response=>{
+      console.log(response);
+      if(response.ok){
+        navigate('/');
+        setIsError(false);
+      }
+      setIsError(true);
+      setErrorMessage("Product details are not valid")
+    }).catch(error=>{
+      console.log(error);
+      setIsError(true);
+      setErrorMessage("Product details are not valid")
     })
   }
   return (
     <div className="parent-container">
       <div className="form-container">
         <form>
+        {isError?<span style={{color:"red",width:"100%"}}> * {errorMessage}</span>:''}
           <label htmlFor="productname">Name </label>
           <input
             type="text"
@@ -90,7 +127,7 @@ function AddProduct() {
             value={productData.prize}
             onChange={(e)=>handleData(e)}
           />
-          <button onClick={postData} style={{backgroundColor:"Blue"}}>Add</button> <Link to="/"><button style={{border:"1px solid black",color:"black"}}>Back</button></Link>
+          <button onClick={id !=undefined ? updateData : postData} style={{backgroundColor:"Blue"}}>{id !=undefined ? "Update" : "Add"}</button> <Link to="/"><button style={{border:"1px solid black",color:"black"}}>Back</button></Link>
         </form> 
       </div>
     </div>
