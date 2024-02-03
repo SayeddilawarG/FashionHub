e62a4e828310194c40494869edd2bfd2f46bafc0
SayeddilawarG <sayed.dilawarg@gmail.com>
2024-02-03 14:23:28 +0530

    changes in library

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dcb1b0ab6e755d7bcae32a75e37ddb2ece0c540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2378f9b1dec42e53b7f5787c34269c5162ea33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
